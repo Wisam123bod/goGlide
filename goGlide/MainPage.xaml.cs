3323913c4f2a80f870de599767325d2ec5d47939
MuratHalilovic <halilovicmurat@gmail.com>
2024-02-22 21:14:37 +0100

    Basic main layout and functions

--- goGlide/MainPage.xaml.cs
+++ goGlide/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace goGlide;
+﻿using System;
+
+namespace goGlide;
 
 public partial class MainPage : ContentPage
 {
@@ -9,17 +11,45 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	
+	private void RegNewCarClicked(object sender, EventArgs e)
 	{
 		count++;
-
+		/**
 		if (count == 1)
 			CounterBtn.Text = $"Clicked {count} time";
 		else
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
+		*/
 	}
+
+    private void ListAllCarsClicked(object sender, EventArgs e)
+    {
+        count++;
+        /**
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+		*/
+    }
+
+    private void RentOutCarClicked(object sender, EventArgs e)
+    {
+        count++;
+        /**
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+		*/
+    }
 }
 
 
