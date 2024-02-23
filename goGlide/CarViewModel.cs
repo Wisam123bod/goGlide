9c565867ddd0f8573c6f1783f3399509eaaea619
MuratHalilovic <halilovicmurat@gmail.com>
2024-02-23 19:32:48 +0100

    Register new car basic but fully functional

---
+++ goGlide/CarViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace goGlide
+{
+	public class Car
+	{
+        public string RegID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+
+        public Car(string regID, string brand, string model, string type)
+        {
+            RegID = regID;
+            Brand = brand;
+            Model = model;
+            Type = type;
+        }
+
+	}
+	public class CarViewModel
+	{
+        public List<Car> CarFleet { get; } = new List<Car>();
+
+        public void RegisterCar(Car newCar)
+        {
+            CarFleet.Add(newCar);
+        }
+    }
+
+}
+
--- goGlide/RegNewCar.xaml.cs
+++ goGlide/RegNewCar.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace goGlide;
+﻿using System;
+
+namespace goGlide;
 
 public partial class RegNewCar : ContentPage
 {
@@ -6,4 +8,26 @@ public partial class RegNewCar : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void CreateCarButton_Clicked(object sender, EventArgs e)
+    {
+        string regID = Entry_regID.Text;
+        string brand = Entry_Brand.Text;
+        string model = Entry_Model.Text;
+        string type = Entry_Type.Text;
+
+        InitializeCar(regID, brand, model, type);
+    }
+
+    private void InitializeCar(string regID, string brand, string model, string type)
+    {
+        CarViewModel carViewModel = new CarViewModel();
+        Car newCar = new Car(regID, brand, model, type);
+        carViewModel.CarFleet.Add(newCar);
+
+        foreach (Car car in carViewModel.CarFleet)
+        {
+            registeredCarsLabel.Text += $"Brand: {car.Brand}, Model: {car.Model}, Type: {car.Type}\n";
+        }
+    }
 }
